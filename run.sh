609d45b3e5811a401c39dbd0f4c298cce54339c6
Leonardo Vieira <69486932+LeonardoVieira1630@users.noreply.github.com>
2022-01-31 13:53:50 -0300

    Uploading filles
    Uploading the fille that has all the near cli calls for the vault_contract.

--- run.sh
+++ run.sh
@@ -11,25 +11,25 @@ echo $CONTRACT_NAME
 
 
 #####Inicializando o contrato #####
-###################################################near call $CONTRACT_NAME new '{"owner_id":"leopollum.testnet"}' --accountId leopollum.testnet
+#near call $CONTRACT_NAME new '{"owner_id":"leopollum.testnet", "vault_shares": 0}' --accountId leopollum.testnet
 
 
 ###### Transferindo token para o contrato de vault #####
-#near call dai.fakes.testnet ft_transfer_call '{"receiver_id": "'exchange.ref-dev.testnet'", "amount": "851830910754170900", "msg": ""}' --account_id=dev-1642185940616-45169719870943 --amount=0.000000000000000000000001 --gas=300000000000000
-#near call eth.fakes.testnet ft_transfer_call '{"receiver_id": "'exchange.ref-dev.testnet'", "amount": "2000000000000000000", "msg": ""}' --account_id=dev-1642185940616-45169719870943 --amount=0.000000000000000000000001 --gas=300000000000000
+#near call dai.fakes.testnet ft_transfer_call '{"receiver_id": "'exchange.ref-dev.testnet'", "amount": "851830910754170900", "msg": ""}' --account_id=$CONTRACT_NAME --amount=0.000000000000000000000001 --gas=300000000000000
+#near call eth.fakes.testnet ft_transfer_call '{"receiver_id": "'exchange.ref-dev.testnet'", "amount": "2000000000000000000", "msg": ""}' --account_id=$CONTRACT_NAME --amount=0.000000000000000000000001 --gas=300000000000000
 
 
 ##### Chamando função de pegar metadata #####
-#near call $CONTRACT_NAME call_meta '{}' --accountId dev-1642185940616-45169719870943
+#near call $CONTRACT_NAME call_meta '{}' --accountId $CONTRACT_NAME
 
 
 ##### Chamando função de registrar usuário #####
-near call $CONTRACT_NAME call_user_register '{"account_id": "dev-1642185940616-45169719870943"}' --accountId dev-1642185940616-45169719870943
+#near call $CONTRACT_NAME call_user_register '{"account_id": "'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME
 
 
 ##### Mandando wnear pra ref #####
-#near call wrap.testnet near_deposit '{}' --accountId dev-1642185940616-45169719870943 --deposit 10
-#near call wrap.testnet ft_transfer_call '{"receiver_id": "exchange.ref-dev.testnet","amount": "10000000000000000000000000","msg": ""}' --accountId dev-1642185940616-45169719870943 --deposit 0.000000000000000000000001 --gas 300000000000000
+#near call wrap.testnet near_deposit '{}' --accountId $CONTRACT_NAME --deposit 10
+#near call wrap.testnet ft_transfer_call '{"receiver_id": "exchange.ref-dev.testnet","amount": "10000000000000000000000000","msg": ""}' --accountId $CONTRACT_NAME --deposit 0.000000000000000000000001 --gas 300000000000000
 
 
 ##### tirando near da ref #####
@@ -37,32 +37,51 @@ near call $CONTRACT_NAME call_user_register '{"account_id": "dev-1642185940616-4
 
 
 ##### Wrap de near #####
-#near call $CONTRACT_NAME near_to_wrap '{"receiver_id": "exchange.ref-dev.testnet", "amount": "1000000000000000000000000", "msg": ""}' --accountId dev-1642185940616-45169719870943  --deposit 0.000000000000000000000001 --gas 300000000000000
+#near call $CONTRACT_NAME near_to_wrap '{"receiver_id": "leopollum.testnet", "amount": "100000000000000000000000", "msg": ""}' --accountId $CONTRACT_NAME  --deposit 0.000000000000000000000001 --gas 300000000000000
 
 
 ##### Swap de wnear para rft #####
-#near call $CONTRACT_NAME call_swap '{"actions": [{"pool_id": 4,"token_in": "wrap.testnet","token_out": "rft.tokenfactory.testnet", "amount_in": "500000000000000000000000", "min_amount_out": "0"}]}' --accountId dev-1642185940616-45169719870943  --deposit 0.000000000000000000000001 --gas 300000000000000
+#near call $CONTRACT_NAME call_swap '{"actions": [{"pool_id": 4,"token_in": "wrap.testnet","token_out": "rft.tokenfactory.testnet", "amount_in": "500000000000000000000000", "min_amount_out": "0"}]}' --accountId $CONTRACT_NAME  --deposit 0.000000000000000000000001 --gas 300000000000000
 
 
 ##### Ja temos wrap near la, agora add liquidez #####
-#near call $CONTRACT_NAME call_add_liquidity '{"pool_id": 193, "amounts": ["851830910754170900", "2000000000000000000"]}' --accountId dev-1642185940616-45169719870943  --deposit 0.000000000000000000000001 --gas 300000000000000
+#near call $CONTRACT_NAME call_add_liquidity '{"pool_id": 193, "amounts": ["2259981532704057266", "2359953913253356717"]}' --accountId $CONTRACT_NAME  --deposit 0.000000000000000000000001 --gas 300000000000000
 
 
 ##### Fazendo stake#####
-#near call $CONTRACT_NAME call_stake '{"receiver_id": "farm110.ref-dev.testnet", "token_id": ":193", "amount": "4608525179932846610798", "msg": ""}' --accountId dev-1642185940616-45169719870943  --deposit 0.000000000000000000000001 --gas 300000000000000
-#near call farm110.ref-dev.testnet list_user_seeds '{"account_id":"dev-1642185940616-45169719870943"}' --accountId dev-1642185940616-45169719870943
+#near call $CONTRACT_NAME call_stake '{"receiver_id": "farm110.ref-dev.testnet", "token_id": ":193", "amount": "4608525179932846610798", "msg": ""}' --accountId $CONTRACT_NAME  --deposit 0.000000000000000000000001 --gas 300000000000000
+#near call farm110.ref-dev.testnet list_user_seeds '{"account_id":"'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME
 
 
 ##### Fazendo claim das rewards ##### 
-#near call $CONTRACT_NAME call_claim '{"seed_id": "exchange.ref-dev.testnet@193"}' --account_id=dev-1642185940616-45169719870943  --gas 300000000000000
+#near call $CONTRACT_NAME call_claim '{"seed_id": "exchange.ref-dev.testnet@193"}' --account_id=$CONTRACT_NAME  --gas 300000000000000
 
-#near view farm110.ref-dev.testnet list_rewards '{"account_id": "dev-1642185940616-45169719870943"}' --account_id=dev-1642185940616-45169719870943
-#near view farm110.ref-dev.testnet get_unclaimed_reward '{"account_id": "dev-1642185940616-45169719870943", "farm_id":"exchange.ref-dev.testnet@193#0"}' --accountId dev-1642185940616-45169719870943
+#near view farm110.ref-dev.testnet list_rewards '{"account_id": "'$CONTRACT_NAME'"}' --account_id=$CONTRACT_NAME
+#near view farm110.ref-dev.testnet get_unclaimed_reward '{"account_id": "'$CONTRACT_NAME'", "farm_id":"exchange.ref-dev.testnet@193#0"}' --accountId $CONTRACT_NAME
 
 
 ##### Fazendo Unstake #####
-#near call $CONTRACT_NAME call_unstake '{"seed_id": "exchange.ref-dev.testnet@193", "amount":"100000" , "msg":""}' --account_id=dev-1642185940616-45169719870943  --gas 300000000000000 --deposit 0.000000000000000000000001
+#near call $CONTRACT_NAME call_unstake '{"seed_id": "exchange.ref-dev.testnet@193", "amount":"100000" , "msg":""}' --account_id=$CONTRACT_NAME  --gas 300000000000000 --deposit 0.000000000000000000000001
 
 
 ##### Withdraw de rewards #####
 #near call $CONTRACT_NAME call_withdraw_reward '{"token_id": "ref.fakes.testnet", "amount":"2140709894097076593" , "unregister":"false"}' --account_id=leopollum.testnet  --gas 300000000000000 --deposit 0.000000000000000000000001
+
+#near call $CONTRACT_NAME call_get_deposits '{"account_id":"'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME  --gas 300000000000000 --deposit 0.000000000000000000000001
+
+
+
+#near call $CONTRACT_NAME storage_deposit '{"account_id": "leopollum.testnet", "registration_only": false}' --accountId leopollum.testnet --gas 300000000000000 --deposit 0.5
+
+#near call $CONTRACT_NAME near_to_wrap '{"account_id": "leopollum.testnet", "receiver_id": "exchange.ref-dev.testnet", "amount": "500000000000000000000000", "msg": ""}' --accountId leopollum.testnet --gas 300000000000000 
+
+#near call $CONTRACT_NAME add_to_vault '{"account_id": "leopollum.testnet", "vault_contract": "'$CONTRACT_NAME'"}' --accountId leopollum.testnet --gas 300000000000000 --deposit 0.01
+
+#near call $CONTRACT_NAME withdraw_of_reward '{"vault_contract": "'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME --gas 300000000000000 --deposit 0.000000000000000000000001
+
+near call $CONTRACT_NAME withdraw_all '{"seed_id": "exchange.ref-dev.testnet@193", "amount": "100000", "msg": "", "vault_contract": "'$CONTRACT_NAME'"}' --accountId leopollum.testnet --gas 300000000000000 
+
+
+#near call exchange.ref-dev.testnet remove_liquidity '{"pool_id": 193, "shares": "100000", "min_amounts": ["1", "1"]}' --accountId $CONTRACT_NAME --amount 0.000000000000000000000001
+
+#near call exchange.ref-dev.testnet withdraw '{"token_id": "", "amount": "900000000000"}' --accountId $CONTRACT_NAME --amount 0.000000000000000000000001
