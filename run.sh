00745327887dc7341f855a585a745cad83de7d09
Leonardo Vieira <69486932+LeonardoVieira1630@users.noreply.github.com>
2022-03-04 10:56:06 -0300

    fix: change the parameter de-...... to $CONTRACT_NAME

--- run.sh
+++ run.sh
@@ -49,10 +49,10 @@ near call $CONTRACT_NAME withdraw_of_reward '{"vault_contract": "'$CONTRACT_NAME
 
 
 #### Start croncat tasks responsible for auto-compound 
-#near call manager_v1.croncat.testnet create_task '{"contract_id": "dev-1645186405351-30060585523052", "function_id": "withdraw_of_reward","cadence": " * * */24 * * *","recurring": true,"deposit": "1","gas": 230000000000000}' --accountId dev-1645538840406-17118853348558 --amount 20 --gas 300000000000000 
-#near call manager_v1.croncat.testnet create_task '{"contract_id": "dev-1645186405351-30060585523052", "function_id": "claim_reward","cadence": " * * */24 * * *","recurring": true,"deposit": "1","gas": 150000000000000}' --accountId dev-1645538840406-17118853348558 --amount 20 --gas 300000000000000 
-#near call manager_v1.croncat.testnet create_task '{"contract_id": "dev-1645186405351-30060585523052", "function_id": "auto_function_1","cadence": " * * */24 * * *","recurring": true,"deposit": "0","gas": 200000000000000}' --accountId dev-1645538840406-17118853348558 --amount 20 --gas 300000000000000
-#near call manager_v1.croncat.testnet create_task '{"contract_id": "dev-1645186405351-30060585523052", "function_id": "auto_function_2","cadence": " * * */24 * * *","recurring": true,"deposit": "0","gas": 230000000000000}' --accountId dev-1645538840406-17118853348558 --amount 20 --gas 300000000000000
+#near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "withdraw_of_reward","cadence": " * * */24 * * *","recurring": true,"deposit": "1","gas": 230000000000000}' --accountId $CONTRACT_NAME --amount 20 --gas 300000000000000 
+#near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "claim_reward","cadence": " * * */24 * * *","recurring": true,"deposit": "1","gas": 150000000000000}' --accountId $CONTRACT_NAME --amount 20 --gas 300000000000000 
+#near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "auto_function_1","cadence": " * * */24 * * *","recurring": true,"deposit": "0","gas": 200000000000000}' --accountId $CONTRACT_NAME --amount 20 --gas 300000000000000
+#near call manager_v1.croncat.testnet create_task '{"contract_id": "'$CONTRACT_NAME'", "function_id": "auto_function_2","cadence": " * * */24 * * *","recurring": true,"deposit": "0","gas": 230000000000000}' --accountId $CONTRACT_NAME --amount 20 --gas 300000000000000
 
 #### Functions managed by auto-compound
 #near call $CONTRACT_NAME withdraw_of_reward '{}' --accountId $CONTRACT_NAME --gas 230000000000000 --deposit 0.000000000000000000000001
